07f8c6fe409680c512e415028dc1697e7af2c222
Arrigo Lupori <arrigo.lupori@gmail.com>
2021-12-19 15:18:57 +0100

    Changed NEXT url

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
 	reactStrictMode: true,
 	env: {
 		API_URL: 'https://arrigodev-cms.herokuapp.com/api',
-		NEXT_URL: 'http://localhost:3000',
+		NEXT_URL: 'https://arrigodev-frontend.vercel.app',
 	},
 }
