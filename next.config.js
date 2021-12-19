fa447cd54c1ab6aa16dfc72532a6708e94310020
Arrigo Lupori <arrigo.lupori@gmail.com>
2021-12-19 15:33:49 +0100

    Changed NEXT url for CORS policy

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	reactStrictMode: true,
 	env: {
 		API_URL: 'https://arrigodev-cms.herokuapp.com/api',
-		NEXT_URL: 'https://arrigodev-frontend.vercel.app',
+		NEXT_URL:
+			'https://cors-proxy-al.herokuapp.com/https://arrigodev-frontend.vercel.app',
 	},
 }
